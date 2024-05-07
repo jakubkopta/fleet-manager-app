30c8c7eb80ea89f2154a2ee6a27f3d04edb69175
Jakub Kopta <jakubkopta@gmail.com>
2024-05-07 11:59:59 +0200

    vehicles

--- fleet-manager-frontend/src/components/ListUserComponent.jsx
+++ fleet-manager-frontend/src/components/ListUserComponent.jsx
@@ -1,13 +1,20 @@
 import {useEffect, useState} from "react";
 import {getAllUsers} from "../services/UserService.js";
+import {getAllVehicles} from "../services/VehicleService.js";
 
 const ListUserComponent = () => {
 
     const [users, setUsers] = useState([]);
+    const [vehicles, setVehicles] = useState([]);
 
     useEffect(() => {
         getAllUsers().then((response) => {
             setUsers(response.data);
+        }).catch(error => {
+            console.log(error);
+        })
+        getAllVehicles().then((response) => {
+            setVehicles(response.data);
         }).catch(error => {
             console.log(error);
         })
@@ -18,25 +25,49 @@ const ListUserComponent = () => {
             <h2 className="text-center">List of users</h2>
             <table className="table">
                 <thead>
-                    <tr>
-                        <th>Id</th>
-                        <th>First Name</th>
-                        <th>Last Name</th>
-                        <th>Email</th>
-                        <th>Password</th>
-                    </tr>
+                <tr>
+                    <th>Id</th>
+                    <th>First Name</th>
+                    <th>Last Name</th>
+                    <th>Email</th>
+                    <th>Phone</th>
+                </tr>
                 </thead>
                 <tbody>
-                    {
-                        users.map(user =>
+                {
+                    users.map(user =>
                         <tr key={user.id}>
                             <td>{user.id}</td>
                             <td>{user.firstName}</td>
                             <td>{user.lastName}</td>
                             <td>{user.email}</td>
-                            <td>{user.password}</td>
+                            <td>{user.phoneNumber}</td>
                         </tr>)
-                    }
+                }
+                </tbody>
+            </table>
+            <h2 className="text-center">List of vehicles</h2>
+            <table className="table">
+                <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Brand</th>
+                    <th>Model</th>
+                    <th>Year</th>
+                    <th>Plate</th>
+                </tr>
+                </thead>
+                <tbody>
+                {
+                    vehicles.map(user =>
+                        <tr key={user.id}>
+                            <td>{user.id}</td>
+                            <td>{user.brand}</td>
+                            <td>{user.model}</td>
+                            <td>{user.year}</td>
+                            <td>{user.plateNumber}</td>
+                        </tr>)
+                }
                 </tbody>
             </table>
         </div>
